39cd9f77d95e6b0638ef7151a98ecebe7c64bf9a
Taras-Roboteckyi <rtvr@ukr.net>
2024-08-16 21:00:44 +0300

    changed settings app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClient, FormsModule],
+  imports: [BrowserModule, HttpClient, FormsModule, CommonModule],
   providers: [],
   bootstrap: [AppComponent],
 })
