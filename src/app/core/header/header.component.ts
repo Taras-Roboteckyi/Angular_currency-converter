b9782bcfc4efeb7d9819daa41844ff4a6045e163
Taras-Roboteckyi <rtvr@ukr.net>
2024-09-12 21:42:27 +0300

    added logic in header component ts

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -3,8 +3,45 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
+  title = 'Currency Converter';
+  amount1 = 0;
+  amount2 = 0;
+  fromCurrency = 'USD';
+  toCurrency = 'UAH';
+  currencies = ['USD', 'EUR', 'UAH'];
 
+  // Метод для обробки зміни суми
+  onAmountChange(newAmount: number, type: 'from' | 'to'): void {
+    if (type === 'from') {
+      this.amount1 = newAmount;
+      this.convert('from');
+    } else {
+      this.amount2 = newAmount;
+      this.convert('to');
+    }
+  }
+
+  // Метод для обробки зміни валюти
+  onCurrencyChange(newCurrency: string, type: 'from' | 'to'): void {
+    if (type === 'from') {
+      this.fromCurrency = newCurrency;
+      this.convert('from');
+    } else {
+      this.toCurrency = newCurrency;
+      this.convert('to');
+    }
+  }
+
+  // Метод для конвертації валют
+  convert(direction: 'from' | 'to'): void {
+    // Логіка конвертації валют
+    if (direction === 'from') {
+      // Рахуємо amount2
+    } else {
+      // Рахуємо amount1
+    }
+  }
 }
